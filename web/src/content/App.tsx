d6685abb7c9cc1d2b9241be325d1d4c71e33f769
Alan <abruckenstein@tradepmr.com>
2019-03-15 15:46:42 -0400

    idk, adding firebase and dealing with gitignore and shit

--- web/src/content/App.tsx
+++ web/src/content/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import Routes from './Routes'
 import { AppStateTypes } from '../redux/store/templates/appState'
+import * as firebase from 'firebase';
 
 const reactLogo = './assets/images/raptor_flipped.png'
 
@@ -17,7 +18,30 @@ interface AppTypes {
     logoClick: () => void;
 }
 
+var config = {
+    apiKey: "AIzaSyBFxlDxENgHf9LM5oMT9bqoURy8cV7nTG8",
+    authDomain: "raptorattacks-526a8.firebaseapp.com",
+    databaseURL: "https://raptorattacks-526a8.firebaseio.com",
+    projectId: "raptorattacks-526a8",
+    storageBucket: "raptorattacks-526a8.appspot.com",
+    messagingSenderId: "238522833662"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
 let currentDaysSinceAttacks = 3;
+let previousDaysSinceAttacks = [{
+    endDate: new Date,
+    numberOfDays: 12
+},
+{
+    endDate: new Date,
+    numberOfDays: 2
+},
+{
+    endDate: new Date,
+    numberOfDays: 7
+}]
 
 const App = (props: AppTypes) => {
     return (
